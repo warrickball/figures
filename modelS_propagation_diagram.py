bd2aab2276b343860c836311d4f663a974cdeb1e
Warrick Ball <wball@phy-hrs-171.ph.bham.ac.uk>
2017-02-14 16:38:15 +0000

    Added shading and annotations to propagation diagram.

--- modelS_propagation_diagram.py
+++ modelS_propagation_diagram.py
@@ -23,16 +23,31 @@ cs2 = G1*P/rho
 
 G = 6.672e-8
 
-N = np.sqrt(G*m/r**3*A)/2./np.pi
-N[np.isinf(N)] = np.nan
-N[np.isnan(N)] = 0.
-N[N<=0.] = 1e-99
-S = [np.sqrt(1.*l*(l+1)*cs2/r**2)/2./np.pi for l in [1,20]]
+N2 = G*m/r**3*A
+N2[N2<0.] = 1e-99  # prevents NaNs from sqrt(-1)
+N = np.sqrt(N2)/2./np.pi
+S = [np.sqrt(1.*l*(l+1)*cs2/r**2)/2./np.pi for l in [1,15]]
 
 pl.plot([-1,2], [5.2e3, 5.2e3], 'k--')
-pl.semilogy(r/R, N*1e6)
+line, = pl.semilogy(r/R, N*1e6)
+pl.fill_between(r/R, 1e-99, N*1e6, facecolor=line.get_color(), alpha=0.5)
 for Si in S:
-    pl.semilogy(r/R, Si*1e6)
+    line, = pl.semilogy(r/R, Si*1e6)
+    pl.fill_between(r/R, Si*1e6, 1e99, facecolor=line.get_color(), alpha=0.5)
+
+pl.text(0.35, 10.**3., '$\ell=1$ pressure modes',
+            horizontalalignment='center',
+            verticalalignment='bottom')
+pl.text(0.8, 10.**3.33, '$\ell=15$ pressure modes',
+            horizontalalignment='center',
+            verticalalignment='center')
+pl.text(0.35, 10.**1.75, 'all gravity modes',
+            horizontalalignment='center',
+            verticalalignment='center')
+pl.text(0.99, 5.5e3, 'acoustic cut-off frequency',
+        horizontalalignment='right',
+        verticalalignment='bottom')
+        
     
 pl.axis([0., np.max(r/R), 1e1, 1e4])
 pl.xlabel('radius (solar units)')

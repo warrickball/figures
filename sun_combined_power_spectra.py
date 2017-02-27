bbd6d003c63f8d820d489863c9708e39ca11d2fc
Warrick Ball <wball@phy-hrs-171.ph.bham.ac.uk>
2017-02-27 12:16:50 +0000

    Reorganized power spectrum plot to save FFT data (for speed).

--- sun_combined_power_spectra.py
+++ sun_combined_power_spectra.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
+"""Intensity and velocity power spectra of the Sun, showing the
+low-degree oscillations.  Data are taken from GOLF and VIRGO/SPM.
+
+"""
+
+
 import numpy as np
 from matplotlib import pyplot as pl
 from astropy.io import fits
+import argparse
 
 # GOLF data
 # Garcia R., Turck-Chieze S., Boumier P. et al. 2005, A&A, 442, 385 
@@ -14,43 +21,58 @@ from astropy.io import fits
 # Jim{\'e}nez, A., Roca Cort{\'e}s, T., \& Jim{\'e}nez-Reyes, S.~J.\ 2002, \solphys, 209, 247
 # http://irfu.cea.fr/Sap/Phocea/Vie_des_labos/Ast/ast_visu.php?id_ast=3581
 
-longnames = ['GOLF_velocity_series_mean_pm1_pm2.fits.gz',
-             'SPM_red_intensity_series.fits',
-             'SPM_green_intensity_series.fits',
-             'SPM_blue_intensity_series.fits']
+parser = argparse.ArgumentParser()
+parser.add_argument('datasets', type=str, nargs='+',
+                    help="List of datasets to be plotted.  Options are " + \
+                    "'golf', 'spm_red,' 'spm_green' or 'spm_blue'.")
+args = parser.parse_args()
 
-shortnames = ['GOLF_mean_pm1_pm2.fits.gz',
-              'SPM_red.fits',
-              'SPM_green.fits',
-              'SPM_blue.fits']
+# the Fourier transforms take some time to calculate, so I do some
+# bookkeepping to make sure we only compute them once
+fftnames = {'golf':      'GOLF_fft.npy',
+            'spm_red':   'SPM_red_fft.npy',
+            'spm_green': 'SPM_green_fft.npy',
+            'spm_blue':  'SPM_blue_fft.npy'}
+longnames = {'golf':      'GOLF_velocity_series_mean_pm1_pm2.fits.gz',
+             'spm_red':   'SPM_red_intensity_series.fits',
+             'spm_green': 'SPM_green_intensity_series.fits',
+             'spm_blue':  'SPM_blue_intensity_series.fits'}
+shortnames = {'golf':      'GOLF_mean_pm1_pm2.fits.gz',
+              'spm_red':   'SPM_red.fits',
+              'spm_green': 'SPM_green.fits',
+              'spm_blue':  'SPM_blue.fits'}
+filenumbers = {'golf':      '1130',
+               'spm_red':   '3581',
+               'spm_green': '3581',
+               'spm_blue':  '3581'}
 
-filenumbers = ['1130','3581','3581','3581']
-
-data = {'GOLF': None, 'red': None, 'green': None, 'blue': None}
-        
-for key, shortname, longname, filenumber in zip(data.keys(), shortnames, longnames, filenumbers):
+dt = 60.0  # cadence, which happens to be the same for all timeseries
+for key in args.datasets:
+    k = key.lower()
     try:
-        data[key] = fits.open('data/%s' % shortname)[0].data
+        y = np.load('data/%s' % fftnames[k])
     except IOError:
-        print('Downloading %s...' % shortname)
-        import urllib2
-        response = urllib2.urlopen('http://irfu.cea.fr/Phocea/file.php?class=astimg&file=%s/%s' % (filenumber, longname))
-        with open('data/%s' % shortname, 'w') as f:
-            f.write(response.read())
+        try:
+            timeseries = fits.open('data/%s' % shortnames[k])[0].data
+        except IOError:
+            print('Downloading %s...' % shortnames[k])
+            import urllib2
+            response = urllib2.urlopen('http://irfu.cea.fr/Phocea/file.php?class=astimg&file=%s/%s' % (filenumber[k], longname[k]))
+            with open('data/%s' % shortnames[k], 'w') as f:
+                f.write(response.read())
             
-        data[key] = fits.open('data/%s' % shortname)[0].data
+            timeseries = fits.open('data/%s' % shortnames[k])[0].data
 
-        
-for key, value in data.iteritems():
-    x = value
-    dt = 60.0
-    t = dt*np.arange(len(x))
+        y = np.fft.rfft(timeseries)
+        np.save('data/%s' % fftnames[k], y)
 
-    y = np.fft.rfft(x)
+    k.lower()
+    t = dt*np.arange(2*len(y)-2)
     df = 1./t[-1]
     f = np.arange(0.0, 0.5/dt+1.1*df, df)
+    yy = np.convolve(np.abs(y)**2, np.ones(10)/10., mode='same')
 
-    pl.loglog(1e3*f, np.abs(y)**2, lw=1.0)
+    pl.loglog(1e3*f, yy, lw=1.0)
 
 pl.xlabel('frequency (mHz)')
 pl.ylabel('power')

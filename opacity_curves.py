c13edb91fde4a5a251d54b95f4c1f1d41ea1cd0f
Warrick Ball <wball@bison.ph.bham.ac.uk>
2017-03-21 21:30:06 +0000

    Added opacity surface and curves.

---
+++ opacity_curves.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import numpy as np
+from matplotlib import pyplot as pl
+from scipy.interpolate import RectBivariateSpline as RBS
+
+key = 'z0.02_x0.7'
+
+with open('/home/wball/mesa/9575/data/kap_data/gs98_%s.data' % key,'r') as f:
+    lines = f.readlines()
+
+opac1 = np.loadtxt(lines[7:])[:,1:].T
+logT1 = np.loadtxt(lines[7:])[:,0]
+logR1 = np.loadtxt(lines[5:6])
+
+with open('/home/wball/mesa/9575/data/kap_data/lowT_fa05_gs98_%s.data' % key,'r') as f:
+    lines = f.readlines()
+
+opac2 = np.loadtxt(lines[7:])[:,1:].T
+logT2 = np.loadtxt(lines[7:])[:,0]
+logR2 = np.loadtxt(lines[5:6])
+
+Rmin = min(np.min(logR1), np.min(logR2))
+Rmax = max(np.max(logR1), np.max(logR2))
+Tmin = min(np.min(logT1), np.min(logT2))
+Tmax = max(np.max(logT1), np.max(logT2))
+
+i1 = np.where(logT1>=4.)[0][0]
+i2 = np.where(logT2<4.)[0][-1]
+
+interpolator1 = RBS(logR1, logT1, opac1)
+interpolator2 = RBS(logR2, logT2, opac2)
+
+for rho in np.arange(-10.,0.1,2.):
+    T = np.linspace(Tmin, Tmax, 200)
+    R = rho - 3.*T + 18.
+    T = T[(R>Rmin)&(R<Rmax)]
+    R = R[(R>Rmin)&(R<Rmax)]
+
+    # c = np.hstack((interpolator2(R[:i2],T[:i2]), interpolator1(R[i1:],T[i1:])))
+    c = [interpolator2(Ri,Ti) for (Ri,Ti) in zip(R,T) if Ti<4.] + \
+        [interpolator1(Ri,Ti) for (Ri,Ti) in zip(R,T) if Ti>=4.]
+    c = np.squeeze(c)
+    pl.plot(T, c, 'k-')
+
+pl.xlabel(r'$\log_{10}(T/\mathrm{K})$')
+pl.ylabel(r'$\log_{10}(\kappa_\mathrm{R}/(\mathrm{cm}^2/\mathrm{g}))$')
+pl.show()
---
+++ opacity_surface.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import numpy as np
+from matplotlib import pyplot as pl
+
+key = 'z0.02_x0.7'
+
+with open('/home/wball/mesa/9575/data/kap_data/gs98_%s.data' % key,'r') as f:
+    lines = f.readlines()
+
+opac1 = np.loadtxt(lines[7:])[:,1:].T
+logT1 = np.loadtxt(lines[7:])[:,0]
+logR1 = np.loadtxt(lines[5:6])
+
+logrho1 = logR1[:,np.newaxis]+3.*logT1-18.
+logT1 = 0.*logR1[:,np.newaxis]+logT1
+i1 = np.where(logT1[0]>=3.85)[0][0]-1
+i1 = 0
+
+opac1 = opac1[:,i1:]
+logrho1 = logrho1[:,i1:]
+logT1 = logT1[:,i1:]
+
+with open('/home/wball/mesa/9575/data/kap_data/lowT_fa05_gs98_%s.data' % key,'r') as f:
+    lines = f.readlines()
+
+opac2 = np.loadtxt(lines[7:])[:,1:].T
+logT2 = np.loadtxt(lines[7:])[:,0]
+logR2 = np.loadtxt(lines[5:6])
+
+logrho2 = logR2[:,np.newaxis]+3.*logT2-18.
+logT2 = 0.*logR2[:,np.newaxis]+logT2
+i2 = np.where(logT2[0]<=3.85)[0][-1]
+i2 = -1
+
+opac2 = opac2[:,:i2]
+logrho2 = logrho2[:,:i2]
+logT2 = logT2[:,:i2]
+
+levels = np.linspace(min(np.min(opac1),np.min(opac2)),
+                     max(np.max(opac1),np.max(opac2)),40)
+
+pl.contourf(logrho1, logT1, opac1, levels)
+pl.contourf(logrho2, logT2, opac2, levels)
+c = pl.contour(logrho1, logT1, opac1, range(0,6),colors='k')
+pl.clabel(c, inline=1, fontsize=8,fmt='%i')
+c = pl.contour(logrho1, logT1, opac1, range(-5,0),colors='k')
+c = pl.contour(logrho2, logT2, opac2, range(-5,0),colors='k')
+pl.clabel(c, inline=1, fontsize=8,fmt='%i')
+c = pl.contour(logrho2, logT2, opac2, range(0,6),colors='k')
+pl.xlabel(r'$\log_{10}(\rho/(\mathrm{g}/\mathrm{cm}^3))$')
+pl.ylabel(r'$\log_{10}(T/\mathrm{K})$')
+pl.show()

d540233a264a2b76142f6798df823f4727bc438a
Warrick Ball <wball@phy-hrs-171.ph.bham.ac.uk>
2017-03-23 13:20:06 +0000

    Changed units of degeneracy plot.  Added help to args.

--- degeneracy.py
+++ degeneracy.py
@@ -4,15 +4,22 @@ import numpy as np
 from matplotlib import pyplot as pl
 from scipy.integrate import quad
 from scipy.optimize import brentq
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawTextHelpFormatter
 
-parser = ArgumentParser()
+parser = ArgumentParser(description=
+"""Plots the momentum occupation fraction for electrons as a function
+of momentum for a single given number density and multiple
+temperatures.  Inspired by Fig. 3.2 of Onno Pols' lecture notes on
+stellar evolution and computed using his equations (3.28)--(3.30).
+
+https://www.astro.ru.nl/~onnop/education/stev_utrecht_notes/""")
+
 parser.add_argument('-T', '--temperature', type=float, nargs='+',
-                    default=[2e6, 2e7])
-parser.add_argument('-n', '--density', type=float, default=6e27)
+                    default=[2e6, 2e7],
+                    help="list of temperatures, in kelvin")
+parser.add_argument('-n', '--density', type=float, default=6e27,
+                    help="number density, in units of cm^{-3}")
 args = parser.parse_args()
-
-# from Onno Pols' notes, Sec. 3.3.5
 
 h = 6.626069e-27
 k = 1.38065e-16
@@ -35,17 +42,19 @@ def n_MB(p, T):
 
 def get_psi(n, T):
     pmax = 2.*m_e*clight
-    return brentq(lambda z: quad(n_e, 0., pmax, args=(z, T))[0]-n, 0., 1e6)
+    return brentq(lambda z: quad(n_e, 0., pmax, args=(z, T))[0]-n, 0., 1e10)
            
 pmax = 2.*m_e*clight
 prange = np.linspace(0., 1e-17, 201)
 n0 = args.density
 
-pl.plot(prange, n_max(prange), 'k-.')
+pl.plot(prange/m_e/clight, n_max(prange)*m_e*clight/n0, 'k-.')
 for T in args.temperature:
     psi = get_psi(n0, T)
 
-    c, = pl.plot(prange, n_e(prange, psi, T))
-    pl.plot(prange, n0*n_MB(prange, T), '--', color=c.get_color())
+    c, = pl.plot(prange/m_e/clight, n_e(prange, psi, T)*m_e*clight/n0)
+    pl.plot(prange/m_e/clight, n0*n_MB(prange, T)*m_e*clight/n0, '--', color=c.get_color())
 
+pl.xlabel('momentum ($m_ec$)')
+pl.ylabel('fractional number density per unit momentum')
 pl.show()

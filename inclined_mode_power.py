f4cc1c4715cbe04d4e36e151957426ff1e415f4f
Warrick Ball <wball@phy-hrs-171.ph.bham.ac.uk>
2017-03-24 09:43:48 +0000

    Hardcoded component powers were broken.  Fixed by using SciPy's associated Legendre polynomials.

--- inclined_mode_power.py
+++ inclined_mode_power.py
@@ -1,47 +1,48 @@
 #!/usr/bin/env python
 
-"""Creates 2D plots of power in an oscillation mode multiplet, as in
-Gizon & Solanki (2003)."""
-
 import numpy as np
 from matplotlib import pyplot as pl
+from scipy.special import lpmn
+from argparse import ArgumentParser
 
-# hardcoded up to l=4 for speed
-energy_list = [
-    [lambda x: 1],
-    [lambda x: np.cos(x)**2, 
-     lambda x: np.sin(x)**2/2.],
-    [lambda x: (3.*np.cos(x)**2-1)**2/4.,
-     lambda x: np.sin(2.*x)**2*3./4.,
-     lambda x: np.sin(x)**4*3./4.],
-    [lambda x: (5.*np.cos(3.*x)+3.*np.cos(x))**2/64.,
-     lambda x: (5.*np.cos(2.*x)+3.)**2*3./64.,
-     lambda x: np.cos(x)**2*np.sin(x)**4*15./8.,
-     lambda x: np.sin(x)**6*5./16.],
-    [lambda x: (35.*np.cos(x)**4-30.*np.cos(x)**2+3.)**2/64.,
-     lambda x: (7./2.*np.sin(4.*x)+np.sin(2.*x))**2*5./256.,
-     lambda x: (7.*np.cos(2.*x)+5.)**2*np.sin(x)**4*5./128.,
-     lambda x: np.cos(x)**2*np.sin(x)**6*35./16.,
-     lambda x: np.sin(x)**8*35./128.]]
+parser = ArgumentParser(description="""Creates 2D plots of power in an oscillation mode multiplet, as in
+Gizon & Solanki (2003).""")
+parser.add_argument('-l', '--ell', type=int, default=2,
+                    help="angular degree")
+parser.add_argument('-w', '--width', type=float, default=0.2,
+                    help="linewidth")
+args = parser.parse_args()
 
+def factorial(n):
+    if n == 0:
+        return 1
+    elif n > 0:
+        return np.prod(range(1, n))
+    else:
+        raise ValueError('can only return factorial of integer >= 0')
 
 def lorentz(x): return 1./(1.+x**2)
 
 def multiplet(x, l, freq, width, splitting, inc):
-    y = energy_list[l][0](inc)*lorentz((x-freq)/width)
-    for m in range(1,l+1):
-        y = y + energy_list[l][abs(m)](inc)*lorentz((x-freq-m*splitting)/width)
-        y = y + energy_list[l][abs(m)](inc)*lorentz((x-freq+m*splitting)/width)
+    legendre2 = lpmn(l, l, np.cos(inc))[0]**2
+    y = lorentz((x-freq)/width)*factorial(l)*legendre2[0][l]
+    for m in range(1, l+1):
+        energy = legendre2[m][l]*factorial(l-m)/factorial(l+m)
+        y = y + lorentz((x-freq-m*splitting)/width)*energy
+        y = y + lorentz((x-freq+m*splitting)/width)*energy
 
     return y
 
-l = 2
+
+l = args.ell
+w = args.width
+
 x = np.linspace(-l-0.5,l+0.5,1000)    # in units of rotational splitting
 inc = np.linspace(0., np.pi/2., 300)
-y = np.vstack([multiplet(x, 2, 0., 0.2, 1., inci) for inci in inc])
+y = np.vstack([multiplet(x, l, 0., w, 1., inci) for inci in inc])
 
-pl.imshow(-y, origin='lower', cmap='gray', extent=[-l-0.5,l+0.5,0,90],
-          aspect='auto')
+pl.imshow(-y, origin='lower', cmap='gray',
+          extent=[-l-0.5, l+0.5, 0, 90], aspect='auto')
 pl.grid('off')
 # pl.xlabel(r'$(\nu-\nu_\mathrm{mode})/\delta\nu_\mathrm{splitting}$')
 pl.xlabel('frequency shift / rotation frequency')

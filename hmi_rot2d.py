1025e4e5d579eab970642c20838207a8e7fb9eb2
Warrick Ball <wball@phy-hrs-171.ph.bham.ac.uk>
2017-04-19 16:09:15 +0100

    Reduced plot to first quadrant, added colourbar.

--- hmi_rot2d.py
+++ hmi_rot2d.py
@@ -3,6 +3,10 @@
 import numpy as np
 from matplotlib import pyplot as pl
 
+from matplotlib.projections import PolarAxes
+from mpl_toolkits.axisartist import floating_axes
+from mpl_toolkits.axisartist.grid_finder import (FixedLocator, MaxNLocator,
+                                                 DictFormatter)
 # data from SDO/HMI webpage
 # http://jsoc.stanford.edu/HMI/Global_products.html
 
@@ -28,17 +32,51 @@ except IOError:
 lat = np.array([15./8.*i for i in np.arange(49)])/180.*np.pi
 r, th = np.meshgrid(rmesh, lat)
 
-fig = pl.figure()
-ax = pl.subplot(111, polar=True)
+# adapted from
+# http://matplotlib.org/examples/axes_grid/demo_floating_axes.html
+fig = pl.gcf()
+tr = PolarAxes.PolarTransform()
+angle_ticks = [(0.0, r"$0^\circ$"),
+               (0.25*np.pi, r"$45^\circ$"),
+               (0.5*np.pi, r"$90^\circ$")]
+grid_locator1 = FixedLocator([v for v, s in angle_ticks])
+tick_formatter1 = DictFormatter(dict(angle_ticks))
 
-# I currently fill out the full circle
-# probably better to just do first quadrant (so symmetry is clear)
-# but this happens to be hard in matplotlib...
-# http://stackoverflow.com/questions/27433310/how-to-clip-polar-plot-in-pylab-pyplot
-Ncontours = 100
-pl.contourf(th, r, rot2d.T, Ncontours)
-pl.contourf(np.pi-th, r, rot2d.T, Ncontours)
-pl.contourf(np.pi+th, r, rot2d.T, Ncontours)
-pl.contourf(2.*np.pi-th, r, rot2d.T, Ncontours)
+grid_locator2 = MaxNLocator(2)
 
+grid_helper = floating_axes.GridHelperCurveLinear(
+    tr, extremes=(0, 0.5*np.pi, 0, 1),
+    grid_locator1=grid_locator1,
+    grid_locator2=grid_locator2,
+    tick_formatter1=tick_formatter1,
+    tick_formatter2=None)
+
+ax1 = floating_axes.FloatingSubplot(fig, 111, grid_helper=grid_helper)
+fig.add_subplot(ax1)
+
+ax1.axis["left"].set_axis_direction("bottom")
+ax1.axis["right"].set_axis_direction("top")
+
+ax1.axis["bottom"].set_visible(False)
+ax1.axis["top"].set_axis_direction("bottom")
+ax1.axis["top"].toggle(ticklabels=True, label=True)
+ax1.axis["top"].major_ticklabels.set_axis_direction("top")
+ax1.axis["top"].label.set_axis_direction("top")
+
+ax1.axis["left"].label.set_text(r'$r/R_\odot$')
+
+# create a parasite axes whose transData in RA, cz
+aux_ax = ax1.get_aux_axes(tr)
+aux_ax.patch = ax1.patch  # for aux_ax to have a clip path as in ax
+ax1.patch.zorder = 0.9  # but this has a side effect that the patch is
+
+Ncontours = 20
+c = aux_ax.contourf(th, r, rot2d.T[::-1], Ncontours)
+pl.colorbar(c)
+
+# plot base of convection zone
+th = np.linspace(0., np.pi/2, 101)
+r = np.ones(len(th))*0.713
+aux_ax.plot(th, r, 'k--')
+ 
 pl.show()

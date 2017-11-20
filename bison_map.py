f56e84a3c2d7c9ae6fec742f65cf3ae2f632544a
Warrick Ball <wball@phy-hrs-171.ph.bham.ac.uk>
2017-11-20 15:30:34 +0000

    Two panels to show complete network

--- bison_map.py
+++ bison_map.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
 
-# based on example at
-# http://scitools.org.uk/cartopy/docs/latest/examples/global_map.html
-
 import matplotlib.pyplot as pl
+import cartopy
 import cartopy.crs as ccrs
-
-ax = pl.axes(projection=ccrs.Robinson())
-ax.set_global()
-
-ax.stock_img()
-ax.coastlines()
 
 # co-ordinates taken from Wikipedia article on BiSON 2017-11-20 11:27
 coords = {'Mount Wilson': (34.22, -118.07),
@@ -20,7 +12,27 @@ coords = {'Mount Wilson': (34.22, -118.07),
           'Carnarvon': (-24.869167, 113.704722),
           'Paul Wild Observatory': (-30.314, 149.562)}
 
+lon0, lat0 = -45, 0
+ax = pl.subplot(1, 2, 1, projection=ccrs.Orthographic(lon0, lat0)) # (lon, lat)
+ax.set_global()
+
+ax.add_feature(cartopy.feature.OCEAN, zorder=0)
+ax.add_feature(cartopy.feature.LAND, zorder=0, edgecolor='black')
+
 for lat, lon in coords.values():
-    pl.plot(lon, lat, 'o', transform=ccrs.PlateCarree())
+    if lon0-90 < lon < lon0+90:
+        pl.plot(lon, lat, 'ro', transform=ccrs.PlateCarree())
+        
+
+lon0, lat0 = 135, 0
+ax = pl.subplot(1, 2, 2, projection=ccrs.Orthographic(lon0, lat0)) # (lon, lat)
+ax.set_global()
+ax.add_feature(cartopy.feature.OCEAN, zorder=0)
+ax.add_feature(cartopy.feature.LAND, zorder=0, edgecolor='black')
+
+for lat, lon in coords.values():
+    if lon0-90 < lon < lon0+90:
+        pl.plot(lon, lat, 'ro', transform=ccrs.PlateCarree())
+
 
 pl.show()

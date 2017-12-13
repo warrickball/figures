d2eef62d36774a8124d8876f287604f9d6c94e33
Warrick Ball <wball@bison.ph.bham.ac.uk>
2017-12-13 21:35:47 +0000

    Added command line options to mode_cut_pyplot

--- mode_cut_pyplot.py
+++ mode_cut_pyplot.py
@@ -5,20 +5,36 @@ from matplotlib import pyplot as pl
 import numpy as np
 from scipy.special import sph_harm, factorial
 from tomso import adipls, io
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('-l', '--ell', type=int, default=20,
+                    help="Angular degree l (default l=20)")
+parser.add_argument('-m', '--emm', type=int, default=16,
+                    help="Azimuthal order m (default m=16)")
+parser.add_argument('-n', '--enn', type=int, default=None,
+                    help="""Radial order n.  You can only choose one of this or the cyclic
+                    frequency n (-f, --freq).  (default n=14)""")
+parser.add_argument('-f', '--freq', type=float, default=None,
+                    help="""Cyclic frequency in mHz.  You can only choose one of this or the
+                    radial order n (-n, --enn).""")
+args = parser.parse_args()
+
+if args.freq and args.enn:
+        raise ValueError("""you can only use one of the radial order (-n,
+        --enn) or the cyclic frequency (-f, --freq)""")
 
 fig = pl.figure(figsize=(6,6))
 ax = fig.add_subplot(111, projection='3d')  # equivalent?
 # ax = pl.gca(projection='3d')  # equivalent?
 fig.subplots_adjust(left=0., right=1., bottom=0., top=1.)
-ell = 20
-emm = 16
 amax = 0.55    # np.sqrt((2.*ell+1.)/(4.*np.pi)*factorial(l-m)/factorial(l+m)) ?
 
 Ntheta = 201
 Nphi = 101
 
 def get_colour(theta, phi):
-    a = sph_harm(emm, ell, 
+    a = sph_harm(args.emm, args.ell, 
                  np.outer(phi, np.ones(len(theta))), 
                  np.outer(np.ones(len(phi)), theta)).real
     a = 0.5+0.5*a/amax
@@ -59,8 +75,14 @@ ax.plot_surface(x, y, z, facecolors=a, **kw)
 
 glob, var = io.load_fgong('data/modelS.fgong')
 css, eigs = adipls.load_amde('data/modelS.amde')
-I = np.where(css['ell']==ell)[0]
-i = I[np.argmin((css['nu_Ri'][I]-3.)**2)]
+I = np.where(css['ell']==args.ell)[0]
+if args.freq:
+    i = I[np.argmin((css['nu_Ri'][I]-args.freq)**2)]
+elif args.enn:
+    i = I[css['enn'][I]==args.enn][0]
+else:
+    i = I[css['enn'][I]==14][0]
+    
 r = eigs[i][:,0][::3]
 y1 = eigs[i][:,1][::3]
 rho = np.interp(r, var[::-1,0]/glob[1], var[::-1,4])
@@ -68,7 +90,7 @@ rho = np.interp(r, var[::-1,0]/glob[1], var[::-1,4])
 # r = np.linspace(0.,1.,51)
 # a = np.outer(np.sin(2.*np.pi*r), np.ones(len(theta)))
 a = np.outer(r*rho**0.5*y1, np.ones(len(theta)))
-a = a*sph_harm(emm, ell, 
+a = a*sph_harm(args.emm, args.ell, 
                np.outer(np.ones(len(r)), np.ones(len(theta))), 
                np.outer(np.ones(len(r)), theta)).real
 a = pl.cm.seismic(0.5+0.5*a/np.max(np.abs(a)))

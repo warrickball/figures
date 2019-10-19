650930c06d316707e9c8eccf9f1cc2d2fb440a58
Warrick Ball <warrickball@gmail.com>
2019-10-19 22:17:20 +0100

    Update modelS_propagation_diagram to use object-oriented FGONG

--- modelS_propagation_diagram.py
+++ modelS_propagation_diagram.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as pl
 # Model S can be downloaded from
 # http://astro.phys.au.dk/~jcd/solar_models/fgong.l5bi.d.15c
 try:
-    glob, var = fgong.load_fgong('data/modelS.fgong')
+    s = fgong.load_fgong('data/modelS.fgong', G=6.67232e-8, return_object=True)
 except IOError:
     try:
         from urllib2 import urlopen
@@ -20,26 +20,18 @@ except IOError:
 
     response.close()
 
-    glob, var = fgong.load_fgong('data/modelS.fgong')
+    s = fgong.load_fgong('data/modelS.fgong', G=6.67232e-8, return_object=True)
         
-M, R = glob[:2]
-r, P, rho, G1, A = var[:-1,[0,3,4,9,14]].T
-m = M*np.exp(var[:-1,1])
-cs2 = G1*P/rho
-
-G = 6.672e-8
-
-N2 = G*m/r**3*A
-N2[N2<0.] = 1e-99  # prevents NaNs from sqrt(-1)
-N = np.sqrt(N2)/2./np.pi
-S = [np.sqrt(1.*l*(l+1)*cs2/r**2)/2./np.pi for l in [1,15]]
+N = np.ones_like(s.N2)*1e-50
+N[s.N2>0] = np.sqrt(s.N2[s.N2>0])/2./np.pi
+S = [np.sqrt(1.*l*(l+1)*s.cs2/s.r**2)/2./np.pi for l in [1,15]]
 
 pl.plot([-1,2], [5.2e3, 5.2e3], 'k--')
-line, = pl.semilogy(r/R, N*1e6)
-pl.fill_between(r/R, 1e-99, N*1e6, facecolor=line.get_color(), alpha=0.5)
+line, = pl.semilogy(s.x, N*1e6)
+pl.fill_between(s.x, 1e-99, N*1e6, facecolor=line.get_color(), alpha=0.5)
 for Si in S:
-    line, = pl.semilogy(r/R, Si*1e6)
-    pl.fill_between(r/R, Si*1e6, 1e99, facecolor=line.get_color(), alpha=0.5)
+    line, = pl.semilogy(s.x, Si*1e6)
+    pl.fill_between(s.x, Si*1e6, 1e99, facecolor=line.get_color(), alpha=0.5)
 
 pl.text(0.35, 10.**3., '$\ell=1$ pressure modes',
             horizontalalignment='center',
@@ -53,9 +45,8 @@ pl.text(0.35, 10.**1.75, 'all gravity modes',
 pl.text(0.99, 5.5e3, 'acoustic cut-off frequency',
         horizontalalignment='right',
         verticalalignment='bottom')
-        
     
-pl.axis([0., np.max(r/R), 1e1, 1e4])
+pl.axis([0., np.max(s.x), 1e1, 1e4])
 pl.xlabel('radius (solar units)')
 pl.ylabel('cyclic frequency ($\mu$Hz)')
 pl.show()

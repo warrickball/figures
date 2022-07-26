66f4f0b358a2ea9bbdbdf0538827f0cee756fe75
Warrick Ball <warrickball@gmail.com>
2022-07-26 09:34:49 +0100

    add options to binding_energy.py

--- binding_energy.py
+++ binding_energy.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python
 
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('--highlight', type=str, nargs='+', default=[],
+                    help="isotopes to highlight, e.g. C12, Fe56")
+parser.add_argument('--xticks', type=int, nargs='+', default=None,
+                    help="place xticks only at these points")
+parser.add_argument('--logx', action='store_true',
+                    help="make x-axis logarithmic")
+parser.add_argument('--logy', action='store_true',
+                    help="make y-axis logarithmic")
+args = parser.parse_args()
+
 import numpy as np
 from matplotlib import pyplot as pl
+
+if args.logx:
+    if args.logy:
+        plotter = pl.loglog
+    else:
+        plotter = pl.semilogx
+else:
+    if args.logy:
+        plotter = pl.semilogy
+    else:
+        plotter = pl.plot
 
 url = 'https://www-nds.iaea.org/amdc/ame2016/mass16.txt'
 filename = 'data/mass16.txt'
@@ -22,7 +46,7 @@ except IOError:
     except ImportError:
         from urllib.request import Request, urlopen
         req = Request(url, headers={'User-Agent' : "Magic Browser"})
-        
+
     response = urlopen(req)
     with open(filename, 'wb') as f:
         f.write(response.read())
@@ -31,7 +55,27 @@ except IOError:
 
     data = np.genfromtxt(filename, dtype=dtype, delimiter=delimiter, skip_header=39)
 
-pl.plot(data['Z'][1:], data['B'][1:]/1e3, 'o') # 1st element is the neutron
+plotter(data['Z'][1:], data['B'][1:]/1e3, 'ko', ms=6, alpha=0.5) # 1st element is the neutron
+
+# create a dict of symbol -> Z
+symbols, I = np.unique(data['symbol'], return_index=True)
+Zs = {symbol.decode().strip(): Z for symbol, Z in zip(symbols, data['Z'][I])}
+
+for h in args.highlight:
+    symbol, A = '', ''
+    for c in h:
+        if c.isalpha():
+            symbol += c
+        else:
+            A += c
+    A = int(A)
+
+    row = data[(data['A']==A)&(data['Z']==Zs[symbol])]
+    plotter(row['Z'], row['B']/1e3, 'o')
+
+if args.xticks:
+    pl.xticks(args.xticks, ['%i' % tick for tick in args.xticks]);
+
 pl.xlabel('atomic number')
 pl.ylabel('binding energy per nucleon (MeV)')
 pl.show()
